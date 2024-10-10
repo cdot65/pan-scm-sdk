ac37bc531cf25d9e7790495f296e7bef1ba4e05a
Calvin Remsburg <calvin@cdot.io>
2024-10-09 20:51:03 -0500

    Add new endpoints and exceptions to the SDK
    Introduced address, address group, and application endpoints to the `API_ENDPOINTS` dictionary. Defined new custom exception classes: `NotFoundError`, and `ConflictError`. All __init__.py files are also updated to ensure they end with a newline.

--- pan_scm_sdk/endpoints/__init__.py
+++ pan_scm_sdk/endpoints/__init__.py
@@ -1,7 +1,15 @@
 # pan_scm_sdk/endpoints/__init__.py
 
+from .addresses import ADDRESSES_ENDPOINTS
+from .address_groups import ADDRESS_GROUPS_ENDPOINTS
+from .applications import APPLICATIONS_ENDPOINTS
+
+# more on the way
+
+
 API_ENDPOINTS = {
-    'get_example': '/example/endpoint',
-    'post_example': '/example/endpoint',
-    # Add other endpoints as needed
+    **ADDRESSES_ENDPOINTS,
+    **ADDRESS_GROUPS_ENDPOINTS,
+    **APPLICATIONS_ENDPOINTS,
+    # more on the way
 }
