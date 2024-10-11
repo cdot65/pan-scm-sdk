ad091b30ca0adf5ea32f0d1c89f7b24646a1c803
Calvin Remsburg <calvin@cdot.io>
2024-10-11 04:48:03 -0500

    Add AddressGroup model and refactor AddressGroups management
    Introduced the AddressGroup model to represent address groups in Palo Alto Networks' Strata Cloud Manager. Updated AddressGroups management methods to utilize the new AddressGroup model, ensuring consistent validation and structure. Additionally, corrected file and class names for better code organization and consistency.

--- scm/config/objects/__init__.py
+++ scm/config/objects/__init__.py
@@ -1,3 +1,4 @@
-# scm/config/__init__.py
+# scm/config/objects/__init__.py
 
-from .addresses import AddressObject
+from .addresses import Addresses
+from .address_groups import AddressGroups
--- scm/models/__init__.py
+++ scm/models/__init__.py
@@ -1,1 +1,4 @@
 # scm/models/__init__.py
+
+from .address import Address
+from .address_group import AddressGroup
