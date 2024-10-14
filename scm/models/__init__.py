91b124e3957df9d216e048bf71a6f5e87df91ea2
Calvin Remsburg <calvin@cdot.io>
2024-10-13 19:39:24 -0500

    Add import for Service models
    Include ServiceRequestModel and ServiceResponseModel to allow for operations related to service entities. This enhances the module's capabilities and ensures that service-related requests and responses are correctly handled.

--- scm/models/__init__.py
+++ scm/models/__init__.py
@@ -3,3 +3,4 @@
 from .address import AddressRequestModel, AddressResponseModel
 from .address_group import AddressGroupRequestModel, AddressGroupResponseModel
 from .application import ApplicationRequestModel, ApplicationResponseModel
+from .service import ServiceRequestModel, ServiceResponseModel
