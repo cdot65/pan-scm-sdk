5816cf8f7cbd12fefe2532b22a77fe3fc7c1b67a
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-12-28 13:04:15 -0600

    Increase max_length for Static Address Group container type
    Updated the maximum allowed character length for the container type of Static Address Group from 255 to 1024. This change accommodates longer names while ensuring flexibility for various use cases.

--- scm/models/objects/address_group.py
+++ scm/models/objects/address_group.py
@@ -75,7 +75,7 @@ class AddressGroupBaseModel(BaseModel):
         None,
         description="Container type of Static Address Group",
         min_length=1,
-        max_length=255,
+        max_length=1024,
         examples=["database-servers"],
     )
     folder: Optional[str] = Field(
